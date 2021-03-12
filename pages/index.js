83d3a1de7b1ccd1afcaca10376b33077d8bc66e3
Ivo <ilegzdins@yahoo.com>
2021-03-12 08:59:13 +0000

    webpack error

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import Awards from '@components/awards';
 import CafeBar from '@components/cafe-bar';
 import HeroSlider from '@components/hero-slider';
-import Mission from '@components/mission';
 
 export default function IndexPage() {
   return (
     <div className=' '>
       <HeroSlider />
-      <Mission />
+
       <CafeBar />
       <Awards />
     </div>
